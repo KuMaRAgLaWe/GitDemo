2cf601345c9c084b38aeebe647839c29bf9598ee
kumar <aglawe.k@gmail.com>
2025-09-16 13:12:40 +0530

    This is commit from Pune

--- src/test/java/mavenProject/test.java
+++ src/test/java/mavenProject/test.java
@@ -1,5 +1,15 @@
 package mavenProject;
 
+import org.testng.annotations.Test;
+
 public class test {
+
+	@Test
+	public void abc() {
+		System.out.println("From India");
+		System.out.println("Hi");
+		System.out.println("I am commited code");
+
+	}
 
 }
